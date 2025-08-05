133a381f64b1f58cb051581ba42855042e5409a5
MertCanCelebi <mertcancelebi12@gmail.com>
2025-08-05 22:28:04 +0300

    Add project files.

---
+++ Entities/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Web.Entities
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string GithubUrl { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
